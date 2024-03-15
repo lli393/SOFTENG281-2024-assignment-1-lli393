98b2edccc7e56a528f11d3447ba5a5dc14f58f37
lli393 <lli393@aucklanduni.ac.nz>
2024-03-15 22:36:12 +1300

    check if venue name is blank

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -5,25 +5,29 @@ import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
   // field
-  int venueAmount = 0;
+  int venueID = 0;
+  String venueName;
+  String venueCode;
+  String capacityInput;
+  String hireFeeInput;
 
   public VenueHireSystem() {}
 
   public void printVenues() {
-    if (venueAmount >= 10) {
+    // print numbers of venue(s)
+    if (venueID >= 10) {
       // it uses “are”, the quantity is a digit, “venues” is plural, and the sentence ends with a
       // colon (“:”)
       MessageCli.NUMBER_VENUES.getMessage(
-          "are", String.valueOf(venueAmount), "s"); // Using the getMessage() method
+          "are", String.valueOf(venueID), "s"); // Using the getMessage() method
       MessageCli.NUMBER_VENUES.printMessage(
-          "are", String.valueOf(venueAmount), "s"); // Using the printMessage() method
-      MessageCli.VENUE_ENTRY.printMessage();
+          "are", String.valueOf(venueID), "s"); // Using the printMessage() method
 
-    } else if (venueAmount > 1) {
+    } else if (venueID > 1) {
       // it uses “are”, the quantity is a word, “venues” is plural, and the sentence ends with a
       // colon (“:”)
       String venueWord = null;
-      switch (venueAmount) {
+      switch (venueID) {
         case 2:
           venueWord = "two";
         case 3:
@@ -44,22 +48,45 @@ public class VenueHireSystem {
       MessageCli.NUMBER_VENUES.getMessage("are", venueWord, "s"); // Using the getMessage() method
       MessageCli.NUMBER_VENUES.printMessage(
           "are", venueWord, "s"); // Using the printMessage() method
-      MessageCli.VENUE_ENTRY.printMessage();
-    } else if (venueAmount == 1) {
+
+    } else if (venueID == 1) {
       // it uses “is”, the quantity is a word, “venue” is singular, and the sentence ends with a
       // colon (“:”)
       MessageCli.NUMBER_VENUES.getMessage("is", "one", ""); // Using the getMessage() method
       MessageCli.NUMBER_VENUES.printMessage("is", "one", ""); // Using the printMessage() method
-      MessageCli.VENUE_ENTRY.printMessage();
-    } else if (venueAmount == 0) {
+
+    } else if (venueID == 0) {
       // the quantity is “no”, “venues” is plural, and the sentence ends with a period (“.”)
       MessageCli.NO_VENUES.printMessage();
+    }
+    // loop for every successfully added venue(s)
+    for (int i = 0; i < venueID; i++) {
+      MessageCli.VENUE_ENTRY.getMessage(venueName, venueCode, capacityInput, hireFeeInput, "");
+      MessageCli.VENUE_ENTRY.printMessage(venueName, venueCode, capacityInput, hireFeeInput, "");
     }
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+    // if venue name is blank
+
+    if (venueName != null && !venueName.isEmpty() && !venueName.trim().isEmpty()) {
+      // venueName is not empty
+      this.venueID = venueID++;
+      this.venueName = venueName;
+      this.venueCode = venueCode;
+      this.capacityInput = capacityInput;
+      this.hireFeeInput = hireFeeInput;
+      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+    } else {
+      // venueName is blank
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+    }
+
+    // if venue code is repeated
+    // venue capacity and hiring fee is whole integer, positive > 0
+    // succesfully create venue if pass all of the above
+
   }
 
   public void setSystemDate(String dateInput) {
